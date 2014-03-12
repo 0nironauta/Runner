f4b61e29e014c46545bc10ffb4dd21d269337da2
marior1989 <m_ruben_rodriguez@hotmail.es>
2014-03-12 20:12:11 -0300

    Runner

--- Assets/levelController.js
+++ Assets/levelController.js
@@ -1,7 +1,11 @@
 ﻿#pragma strict
-
+public var cuboPrefab1 : GameObject;
+public var cuboPrefab2 : GameObject;
+public var cuboPrefab3 : GameObject;
 function Start () {
-
+Instantiate(cuboPrefab1, Vector3(-1,0,0), Quaternion.identity);
+Instantiate(cuboPrefab2, Vector3(0,0,0), Quaternion.identity);
+Instantiate(cuboPrefab3, Vector3(1,0,0), Quaternion.identity);
 }
 
 function Update () {
