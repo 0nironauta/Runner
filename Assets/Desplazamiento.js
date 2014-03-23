b49a50f9634cfb9ade76e9b1d6ab7fb9f958d3dd
0nironauta <sr0@Sr0-Server.local>
2014-03-23 20:39:09 +0100

    Cambios en los 2 scripts
    Cambios para que los cubos se instancien cada 2 segundos y avancen.

--- Assets/Desplazamiento.js
+++ Assets/Desplazamiento.js
@@ -1,12 +1,15 @@
 ﻿#pragma strict
 
-function Start () {
-
-}
+// Una variable pública que podrás ver en el inspector.
+// Este script va en el prefab del cubo, no en la cámara
+public var velocidad : int = -1;
 
 function Update () {
 	
-		transform.Translate(Vector3(-1,1,4) * Time.deltaTime);
-		
+		// Desplazamos el cubo (todos los cubos que están en escena ejecutarán este script)
+		transform.Translate(Vector3(0,0,velocidad) * Time.deltaTime);
+
+		// Si el cubo no se renderiza en ninguna cámara, lo destruimos
+		if (renderer.isVisible == false) Destroy(gameObject);
 	
 }
--- Assets/levelController.js
+++ Assets/levelController.js
@@ -1,25 +1,33 @@
 ﻿#pragma strict
 public var cuboPrefab1 : GameObject;
+
+// Creamos 2 variables para controlar el tiempo
+private var tiempoInstancia    : float = 2.0; // Cada cuántos segundos se instanciarán nuevos cubos
+private var siguienteInstancia : float = 0.0; // Controlará cuándo toca volver a instanciar
+
+/*
+
+Función Start no la vamos a usar por ahora. La comento pero la puedes borrar
+
 function Start () {
-Instantiate(cuboPrefab1, Vector3(-1,1,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(-1,1,2), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(-1,1,4), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(-1,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(0,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(1,0,0), Quaternion.identity);
-
-Instantiate(cuboPrefab1, Vector3(-1,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(0,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(1,0,0), Quaternion.identity);
-
-Instantiate(cuboPrefab1, Vector3(-1,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(0,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(1,0,0), Quaternion.identity);
-
-Instantiate(cuboPrefab1, Vector3(-1,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(0,0,0), Quaternion.identity);
-Instantiate(cuboPrefab1, Vector3(1,0,0), Quaternion.identity);
+	Instantiate(cuboPrefab1, Vector3(-2,1,0), Quaternion.identity);
+	Instantiate(cuboPrefab1, Vector3( 0,1,0), Quaternion.identity);
+	Instantiate(cuboPrefab1, Vector3( 2,1,0), Quaternion.identity);
 }
+*/
 
-function Update () {
+// Usamos la función FixedUpdate, que se ejecuta 1 vez por frame
+function FixedUpdate(){
+
+	// Time.time da el tiempo en segundos desde que comenzó el juego
+	// Cada vez que instanciamos, la variable siguienteInstancia crece, 
+	// por lo que esta condición se cumple cada 2 segundos (el valor de tiempoInstancia)
+	if (Time.time > siguienteInstancia){
+		siguienteInstancia = siguienteInstancia + tiempoInstancia;
+
+		// Instanciamos los 3 cubos:
+		Instantiate(cuboPrefab1, Vector3(-2,1,0), Quaternion.identity);
+		Instantiate(cuboPrefab1, Vector3( 0,1,0), Quaternion.identity);
+		Instantiate(cuboPrefab1, Vector3( 2,1,0), Quaternion.identity);
+	}
 }
